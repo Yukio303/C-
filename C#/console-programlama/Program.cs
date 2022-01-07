1b509e855edcb947361d2c77a27b0b426fb951e9
Yukio303 <l.keremaka@gmail.com>
2022-01-07 12:27:56 +0300

    changes

--- C#/console-programlama/Program.cs
+++ C#/console-programlama/Program.cs
@@ -1,5 +1,4 @@
-<<<<<<< HEAD:console-programlama/Program.cs
-﻿using System;
+using System;
 
 namespace console_programlama
 {
@@ -19,25 +18,3 @@ namespace console_programlama
         }
     }
 }
-=======
-﻿using System;
-
-namespace console_programlama
-{
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
-            Console.WriteLine("Adınızı giriniz:");
-            string name = Console.ReadLine();
-            Console.WriteLine("Soyadınızı giriniz:");
-            string surname = Console.ReadLine();
-
-            Console.WriteLine("Merhaba " + name + " " + surname);
-
-
-        }
-    }
-}
->>>>>>> cadf0a2454db53d20c2ba37a290c941db954a334:C#/console-programlama/Program.cs
