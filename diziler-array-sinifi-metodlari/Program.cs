11248fc6f1ef108203b717f9974d42a143bb5a7d
Yukio303 <l.keremaka@gmail.com>
2022-01-08 18:08:00 +0300

    master

---
+++ diziler-array-sinifi-metodlari/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+namespace diziler_array_sinifi_metodlari
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Sort
+            int[] sayiDizisi = {23,12,4,86,72,3,11,17};
+            
+            Console.WriteLine("****Sırasız dizi****");
+            foreach (var sayi in sayiDizisi)
+            {
+                Console.WriteLine(sayi);
+            }
+            Console.WriteLine("****Sıralı dizi****");
+            Array.Sort(sayiDizisi);
+            foreach (var sayi in sayiDizisi)
+            {
+                Console.WriteLine(sayi);
+            }
+
+            //Clear
+            Console.WriteLine("****Array clear****");
+            //sayiDizisi elemanlarını kullnarak 2. indexten itibaren iki tane elemanı sıfırlar
+            Array.Clear(sayiDizisi,2,2);
+            foreach (var sayi in sayiDizisi)
+            {
+                Console.WriteLine(sayi);
+            }
+
+            //Reverse
+            Console.WriteLine("****Array reverse****");
+            Array.Reverse(sayiDizisi);
+            foreach (var sayi in sayiDizisi)
+            {
+                Console.WriteLine(sayi);
+            }
+
+            //Indexof
+            Console.WriteLine("****Array indexof****"); 
+            Console.WriteLine(Array.IndexOf(sayiDizisi,23));
+
+            //Resize
+            Console.WriteLine("****Array resize****");
+            Array.Resize<int>(ref sayiDizisi,9);
+            sayiDizisi[8] = 99;
+            foreach (var sayi in sayiDizisi)
+            {
+                Console.WriteLine(sayi);
+            } 
+
+
+
+        }
+
+    }
+}
