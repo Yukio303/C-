dac59d939ea6d8203cacb2ca842c83df717b3d80
Yukio303 <l.keremaka@gmail.com>
2022-01-13 16:47:34 +0300

    master

---
+++ odev-1/part-2/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+
+namespace part_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Lütfen iki pozitif sayı giriniz: ");
+            
+            int n = int.Parse(Console.ReadLine());
+            if(n<1)
+            {
+                 Console.WriteLine("Lütfen pozitif bir sayı giriniz: ");
+                n = int.Parse(Console.ReadLine());
+            }
+               
+
+            int m = int.Parse(Console.ReadLine());
+            
+            if(m<1)
+            {
+                Console.WriteLine("Lütfen pozitif bir sayı giriniz: ");
+                m = int.Parse(Console.ReadLine());
+            }
+                
+
+            Console.WriteLine($"Lütfen {n} tane pozitif sayı giriniz: ");
+            
+            int[] list = new int[n];
+
+            for (int i = 0; i < n; i++)
+            {
+                int sayi = int.Parse(Console.ReadLine());
+
+                if(sayi<1)
+                {
+                    Console.WriteLine("Lütfen pozitif bir sayı giriniz: ");
+                    i--;
+                }
+                else
+                {
+                    list[i] = sayi;
+                }     
+            }
+
+            Console.WriteLine("****Girdiğiniz sayıya tam bölünenler****");
+            foreach (var sayi in list)
+            {
+                if(sayi%m == 0)
+                {
+                    Console.WriteLine(sayi);
+                }
+            }
+            
+        }
+
+    }
+
+}   
+
+        
+
+
+    
+
+
