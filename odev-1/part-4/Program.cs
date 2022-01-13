c5cfc5efdcc10fa3c81685f349c6079bd980a841
Yukio303 <l.keremaka@gmail.com>
2022-01-13 17:35:19 +0300

    master

---
+++ odev-1/part-4/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace part_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Lütfen bir cümle yazınız: ");
+
+            string cumle = Console.ReadLine();
+
+            char[] list = cumle.ToCharArray();
+
+            int harf = 0;
+            int kelime = 1;
+
+            foreach (var karakter in list)
+            {
+                if(karakter == ' ')
+                {
+                    kelime++;
+                }
+
+                else if(Char.IsPunctuation(karakter))
+                {
+                    continue;
+                }
+
+                else
+                {
+                    harf++;
+                } 
+            }
+
+            Console.WriteLine("****Sonuçlar****");
+            Console.WriteLine($"Cümlede {kelime} tane kelime vardır.");
+            Console.WriteLine($"Cümlede {harf} tane harf vardır.");
+
+        }
+    }
+}
